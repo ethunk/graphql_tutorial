7453d868deb7a36012c5caa70495b57968c69aa7
eric <eric@coherentpath.com>
2018-12-06 10:41:30 -0500

    hit an error...not working...saving progress

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -3,8 +3,12 @@ import graphene
 import links.schema
 
 
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
 class Query(links.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(links.schema.Mutation, graphene.ObjectType):
+    pass
--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -14,3 +14,28 @@ class Query(graphene.ObjectType):
 
     def resolve_links(self, info, **kwargs):
         return(Link.objects.all())
+
+
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        url = graphene.String()  # Data sent by the user/frontend to the server
+        description = graphene.String()  # Data sent by the user/frontend to the server
+
+        def mutate(self, info, url, description):
+            # Creates a new link object/record in the database using the params passed from the frontend to the server
+            link = Link(url=url, description=description)
+            link.save()
+
+            return CreateLink(  # Returns an object/class with data back to the frontend
+                id=link.id,
+                url=link.url,
+                description=link.description,
+            )
+
+
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
