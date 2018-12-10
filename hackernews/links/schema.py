92658943a8b7246cf2adaf088ba02ae021fe9cfa
eric <eric@coherentpath.com>
2018-12-10 16:47:25 -0500

    next step in tutorial

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -20,20 +20,33 @@ class VoteType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     # Add the search parameter inside our links field
-    links = graphene.List(LinkType, search=graphene.String())
+    links = graphene.List(
+        LinkType,
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    )
     votes = graphene.List(VoteType)
 
     # Change the resolver
-    def resolve_links(self, info, search=None, **kwargs):
+    def resolve_links(self, info, search=None, first=None, skip=None, **kwargs):
         # The value sent with the search parameter will be in the args variable
+        qs = Link.objects.all()
+
         if search:
             filter = (
                 Q(url__icontains=search) |
                 Q(description__icontains=search)
             )
-            return Link.objects.filter(filter)
+            qs = qs.filter(filter)
 
-        return Link.objects.all()
+        if skip:
+            qs = qs[skip:]
+
+        if first:
+            qs = qs[:first]
+
+        return qs
 
     def resolve_votes(self, info, **kwargs):
         return Vote.objects.all()
