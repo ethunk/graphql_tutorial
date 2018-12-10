97552fce6ec12930ae29c6c37d963448219b23ed
eric <eric@coherentpath.com>
2018-12-10 14:06:05 -0500

    moved on to next phase of tutorial

--- hackernews/users/schema.py
+++ hackernews/users/schema.py
@@ -9,6 +9,21 @@ class UserType(DjangoObjectType):
         model = get_user_model()
 
 
+class Query(graphene.ObjectType):
+    me = graphene.Field(UserType)
+    users = graphene.List(UserType)
+
+    def resolve_users(self, info):
+        return get_user_model().objects.all()
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Not Logged In")
+
+        return user
+
+
 class CreateUser(graphene.Mutation):
     user = graphene.Field(UserType)
 
